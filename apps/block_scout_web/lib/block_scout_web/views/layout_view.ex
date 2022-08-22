b2cd30292eb28380f34f0d70dbaf895c7b6cd2da
jkamino <kamino@centaurusworks.jp>
2022-08-22 06:47:50 +0000

    feat: update logo image

--- apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
@@ -37,12 +37,12 @@ defmodule BlockScoutWeb.LayoutView do
   alias BlockScoutWeb.SocialMedia
 
   def logo do
-    Keyword.get(application_config(), :logo) || "/images/ceth_logo.png"
+    Keyword.get(application_config(), :logo) || "/images/sanpo_logo.png"
   end
 
   def logo_footer do
     Keyword.get(application_config(), :logo_footer) || Keyword.get(application_config(), :logo) ||
-      "/images/ceth_logo.png"
+      "/images/sanpo_logo.png"
   end
 
   def logo_text do
